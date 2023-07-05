82d3731ebd0316105d6b70701ef991d1df85c0af
Wangyn <wangyn.80@qq.com>
2023-07-05 15:12:35 +0800

    @feature 打包脚本，一键打包安装exe

--- pack.sh
+++ pack.sh
@@ -1,1 +1,10 @@
-pyinstaller -n ge-rfid-wms -F -w -i icon.ico app.py
+# pyinstaller -n ge-rfid-wms -F -w -i icon.ico app.py
+
+source_file="dist"
+zip_file="ge-rfid-wms.zip"
+if [ -f "$zip_file" ]; then
+  rm "$zip_file"
+fi
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::CreateFromDirectory('$source_file', '$zip_file', 'Optimal', 0); }"
+
+makensis.exe create_shortcut.nsi
